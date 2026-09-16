cb3d8bcc3e55641a5bbc45102c24f08754496946
sudag@revolvesys.co.jp <sudag@revolvesys.co.jp>
2026-09-16 08:22:00 +0000

    ブロンズ完成

---
+++ 成果物/bronze.py
@@ -0,0 +1,15 @@
+def csv_to_bronze(spark, table_name, file_path):
+    df = spark.read\
+         .format("csv")\
+         .option("header", "true")\
+         .option("inferSchema", "true")\
+         .load(file_path)
+    
+    df.write\
+        .format("delta")\
+        .mode("overwrite")\
+        .saveAsTable(f"hands_on.bronze.{table_name}")
+    
+
+
+   
