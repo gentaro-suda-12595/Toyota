0b6231ce2f30099b4b6d85cf1f628fb0023bcc56
sudag@revolvesys.co.jp <sudag@revolvesys.co.jp>
2026-09-17 01:38:54 +0000

    シルバー書き込み共通関数化

--- 成果物/silver_customers.py
+++ 成果物/silver_customers.py
@@ -1,12 +1,10 @@
 from pyspark.sql import functions as F
+from write_silver import write_silver
+
 df_bronze_customers = spark.read.table("hands_on.bronze.bronze_customers")
 
 df_silver_customers = df_bronze_customers\
     .withColumn("customer_state",F.upper(F.col("customer_state")))\
     .withColumn("customer_city",F.upper(F.col("customer_city")))
 
-
-df_silver_customers.write\
-    .format("delta")\
-    .mode("overwrite")\
-    .saveAsTable("hands_on.silver.silver_customers")
+write_silver(spark ,df_silver_customers, "silver_customers")
